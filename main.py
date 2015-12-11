69de940f87fdfa823f187d669a451de28340d238
Doran Cals <dcals001@ucr.edu>
2015-12-10 19:34:30 -0800

    Added verb and noun phrases to Python

--- main.py
+++ main.py
@@ -13,6 +13,23 @@ if __name__ == '__main__':
     parser.add_argument('PossList', nargs='?', default='poss.txt', type=str, help='Path to a list of possessives')
     args = parser.parse_args()
     
+    #name = [line.rstrip('\n') for line in open(sys.stdin)]
     verb = [line.rstrip('\n') for line in open(args.VerbList)]
     noun = [line.rstrip('\n') for line in open(args.NounList)]
     poss = [line.rstrip('\n') for line in open(args.PossList)]
+    
+    # Object phrases should pull from the same list, so stick 'em together
+    possStart = len(noun)
+    noun.extend(poss)
+    
+    #print random.choice(verb) + random.choice(noun)
+    
+    v = random.randrange(len(verb) )
+    p = random.randrange(len(noun) )
+    o = ''
+    if p >= possStart:
+	o = ' ' + 'Haleigh'
+
+    
+    print '{0}{1}{2}{3}'.format(
+	'Doran', verb[v], o, noun[p])
