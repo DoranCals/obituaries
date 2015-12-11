f1cc9fd7df47259d1147d2ba5cab7cbfda5206e9
Doran Cals <dcals001@ucr.edu>
2015-12-10 20:11:07 -0800

    Implemented Python script, added names list

--- main.py
+++ main.py
@@ -3,33 +3,47 @@
 import os
 import sys
 import argparse
+import fileinput
 import random
 
 
+def buildPhrase(nameList, verbList, nounList, possStart):
+    
+    names = random.sample(nameList, 2)
+    verb = random.choice(verbList)
+    n = random.randrange(len(nounList) )
+    noun = nounList[n]
+    
+    if n >= possStart:
+	# ex: "Alice was killed by Bob's gun."
+	result =  '{Name}{Verb} {Name2}{Noun}'.format(
+	    Name = names[0], Verb = verb, Name2 = names[1], Noun = noun)
+    else:
+	# ex: "Alice was killed by bullets."
+	result =  '{Name}{Verb}{Noun}'.format(
+	    Name = names[0], Verb = verb, Noun = noun)
+    
+    return result
+
+
 if __name__ == '__main__':
+    # Optional arguments for input files
     parser = argparse.ArgumentParser(description='Obituaries')
     parser.add_argument('VerbList', nargs='?', default='verb.txt', type=str, help='Path to a list of verb phrases')
     parser.add_argument('NounList', nargs='?', default='noun.txt', type=str, help='Path to a list of noun phrases')
     parser.add_argument('PossList', nargs='?', default='poss.txt', type=str, help='Path to a list of possessives')
     args = parser.parse_args()
     
-    #name = [line.rstrip('\n') for line in open(sys.stdin)]
-    verb = [line.rstrip('\n') for line in open(args.VerbList)]
-    noun = [line.rstrip('\n') for line in open(args.NounList)]
-    poss = [line.rstrip('\n') for line in open(args.PossList)]
-    
+    # By using standard input, entering a small list of nmes is easier.
+    nameList = []
+    for line in sys.stdin:
+	nameList.append(line.rstrip('\n') )
+    #nameList = [line.rstrip('\n') for line in fileinput.input()]
+    verbList = [line.rstrip('\n') for line in open(args.VerbList)]
+    nounList = [line.rstrip('\n') for line in open(args.NounList)]
+    possList = [line.rstrip('\n') for line in open(args.PossList)]
     # Object phrases should pull from the same list, so stick 'em together
-    possStart = len(noun)
-    noun.extend(poss)
+    possStart = len(nounList)
+    nounList.extend(possList)
     
-    #print random.choice(verb) + random.choice(noun)
-    
-    v = random.randrange(len(verb) )
-    p = random.randrange(len(noun) )
-    o = ''
-    if p >= possStart:
-	o = ' ' + 'Haleigh'
-
-    
-    print '{0}{1}{2}{3}'.format(
-	'Doran', verb[v], o, noun[p])
+    print buildPhrase(nameList, verbList, nounList, possStart)
